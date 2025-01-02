cfdc0b4c17c46e23b1fc3edae8884ed7b257f13e
Casuledda89 <valentina.casula70@gmail.com>
2025-01-02 19:40:04 +0100

    predisposizione rotte di ripristino password

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -15,7 +15,17 @@ import loginController from './user/loginController.js';
 const setup = (app) => {
     app.post('/user', createUserValidator, createUserController);
     app.get('/user/:id/confirm/:registrationToken',checkUserMailController);
-    app.post('/user/login', loginValidator, loginController)
+    app.post('/user/login', loginValidator, loginController);
+
+
+    // controllo e spedico l'eventuale link di ripristino password sul FE
+    app.get('/user/recovery-password/:email' , recoveryPasswordController);
+
+    //controllo che il token di reset password sia ancora valido e lo dico al FE
+    app.get('/user/:id/restore/:restoreToken', validRestoreTokenController)
+
+    //aggiorno la password per l'utente
+    app.post('/user/reset-password/:id/:restoreToken', resetPasswordValidator, updateUserPasswordController);
     
     //definire app.use dopo la route app.post, app.patch
     app.use((err, req, res, next) => {
